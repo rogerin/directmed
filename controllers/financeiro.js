aebb301edf040949421253802b18893f7c067e36
Rogerio Alencar Filho <geriofilho@gmail.com>
2014-07-24 01:18:45 -0300

    traycheckout

--- controllers/financeiro.js
+++ controllers/financeiro.js
@@ -106,13 +106,21 @@ module.exports = function(app){
 							        } else {
 							        	console.log('ATUALIZADO COM SUCESSO!');
 
+							        	var mongoose = require('mongoose');
+										var Schema = mongoose.Schema;
+										var ObjectId = Schema.ObjectId; 
 
+										var meuId = new ObjectId(req.body.transaction.order_number);
 
-							        	Cobranca.findById(req.body.transaction.order_number, function(err, c){
+							        	Cobranca.findById(meuId, function(err, c){
 							        		if(err) { console.log(err);}
 							        		else {
+
+
+												var id = new ObjectId(resp._id);
+
 							        	
-												c.resposta 		= resp;
+												c.resposta 		= id;
 												c.status_id 	= req.body.transaction.status_id;
 												c.status_name 	= req.body.transaction.status_name;
 												c.save(function(err){
